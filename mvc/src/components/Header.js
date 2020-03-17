09edb04105b115fb11e7b9fcd0da0a45aec88f51
fenglingcong <1539027251@qq.com>
2020-03-17 21:29:01 +0800

    'update'

--- mvc/src/components/Header.js
+++ mvc/src/components/Header.js
@@ -4,7 +4,15 @@ import TodoTextInput from './TodoTextInput';
 const Header = () => (
   <header className="header">
     <h1>todos</h1>
-    <TodoTextInput />
+    <TodoTextInput
+      newTodo
+      onSave={(text) => {
+        if (text.length !== 0) {
+          // something
+        }
+      }}
+      placeholder="what needs to be done?"
+    />
   </header>
 )
 
