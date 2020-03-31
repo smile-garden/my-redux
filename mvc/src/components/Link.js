e9217ff9740815e4657b844865fb6e29abaa95c9
fenglingcong <1539027251@qq.com>
2020-03-31 17:21:40 +0800

    'eslint'

--- mvc/src/components/Link.js
+++ mvc/src/components/Link.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
 const Link = ({ active, children, setFilter }) => (
+  // eslint-disable-next-line jsx-a11y/anchor-is-valid
   <a
     className={classnames({ selected: active })}
     style={{ cursor: 'pointer' }}
