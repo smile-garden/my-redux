9d51047301b2e79d436be7a82bc09ac2a2552718
fenglingcong <1539027251@qq.com>
2020-03-31 16:38:24 +0800

    Revert "'end'"
    This reverts commit 2ca03b022b3c33b33301d43841a448b6f3e2feb9.

--- mvc/src/components/MainSection.js
+++ mvc/src/components/MainSection.js
@@ -1,38 +1,13 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import VisibleTodoList from '../containers/VisibleTodoList';
+import TodoList from './TodoList';
 import Footer from './Footer';
 
-const MainSection = ({ todosCount, completedCount, actions }) => (
+const MainSection = () => (
   <section className="main">
-    {
-      !!todosCount &&
-      <span>
-        <input
-          className="toggle-all"
-          type="checkbox"
-          checked={completedCount === todosCount}
-          readOnly
-        />
-        <label onClick={actions.completeAllTodos} />
-      </span>
-    }
-    <VisibleTodoList />
-    {
-      !!todosCount &&
-      <Footer
-        completedCount={completedCount}
-        activeCount={todosCount - completedCount }
-        onClearCompleted={actions.clearCompleted}
-      />
-    }
+    主要内容
+    <TodoList />
+    <Footer />
   </section>
 )
-
-MainSection.propTypes = {
-  todosCount: PropTypes.number.isRequired,
-  completedCount: PropTypes.number.isRequired,
-  actions: PropTypes.object.isRequired,
-}
 
 export default MainSection;
--- mvc/src/components/TodoList.js
+++ mvc/src/components/TodoList.js
@@ -1,26 +1,10 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import TodoItem from './TodoItem';
 
-const TodoList = ({ filteredTodos, actions}) => (
+const TodoList = () => (
   <ul className="todo-list">
-    {filteredTodos.map(todo =>
-      <TodoItem
-        key={todo.id}
-        todo={todo}
-        {...actions}
-      />
-    )}
+    <TodoItem />
   </ul>
 )
-
-TodoList.propTypes = {
-  filteredTodos: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    completed: PropTypes.bool.isRequired,
-    text: PropTypes.string.isRequired,
-  }).isRequired).isRequired,
-  actions: PropTypes.object.isRequired,
-};
 
 export default TodoList;
