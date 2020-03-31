1305b1a1595a25ccbfbf97f2e2df2ff7b6117acc
fenglingcong <1539027251@qq.com>
2020-03-31 19:47:13 +0800

    '333'

--- mvc/src/components/MainSection.js
+++ mvc/src/components/MainSection.js
@@ -1,13 +1,38 @@
 import React from 'react';
-import TodoList from './TodoList';
+import PropTypes from 'prop-types';
+import VisibleTodoList from '../containers/VisibleTodoList';
 import Footer from './Footer';
 
-const MainSection = () => (
+const MainSection = ({ todosCount, completedCount, actions }) => (
   <section className="main">
-    主要内容
-    <TodoList />
-    <Footer />
+    {
+      !!todosCount &&
+      <span>
+        <input
+          className="toggle-all"
+          type="checkbox"
+          checked={completedCount === todosCount}
+          readOnly
+        />
+        <label onClick={actions.completeAllTodos} />
+      </span>
+    }
+    <VisibleTodoList />
+    {
+      !!todosCount &&
+      <Footer
+        completedCount={completedCount}
+        activeCount={todosCount - completedCount }
+        onClearCompleted={actions.clearCompleted}
+      />
+    }
   </section>
 )
+
+MainSection.propTypes = {
+  todosCount: PropTypes.number.isRequired,
+  completedCount: PropTypes.number.isRequired,
+  actions: PropTypes.object.isRequired,
+}
 
 export default MainSection;
--- mvc/src/components/TodoList.js
+++ mvc/src/components/TodoList.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import TodoItem from './TodoItem';
 
-const TodoList = () => (
+const TodoList = ({ filteredTodos, actions}) => (
   <ul className="todo-list">
-    <TodoItem />
+    {filteredTodos.map(todo =>
+      <TodoItem
+        key={todo.id}
+        todo={todo}
+        {...actions}
+      />
+    )}
   </ul>
 )
+
+TodoList.propTypes = {
+  filteredTodos: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    completed: PropTypes.bool.isRequired,
+    text: PropTypes.string.isRequired,
+  }).isRequired).isRequired,
+  actions: PropTypes.object.isRequired,
+};
 
 export default TodoList;
